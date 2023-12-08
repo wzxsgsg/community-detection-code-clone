d23e20a30486073b710571c42c9b40411959bb9e
wzxsgsg <52185117+wzxsgsg@users.noreply.github.com>
2023-12-08 09:47:50 +0800

    Update attention_LSTM.py

--- paper_code/attention_LSTM.py
+++ paper_code/attention_LSTM.py
@@ -33,32 +33,16 @@ class SiameseLSTM(nn.Module):
 #             torch.nn.Dropout(0.2),
             torch.nn.ReLU(),
             nn.Linear(hidden_size, output_size))
-#         self.fc1 = nn.Sequential(
-#             torch.nn.ReLU(),
-#             nn.Linear(hidden_size, output_size))
-#         self.fc2 = nn.Sequential(
-#             torch.nn.ReLU(),
-#             nn.Linear(hidden_size, output_size))
 
     def forward(self, input1, input2):
         input1 = self.layer_norm(input1)
         input2 = self.layer_norm(input2)
         output_1, (h_n_1, c_n_1) = self.lstm(input1)
         output_2, (h_n_2, c_n_2) = self.lstm(input2)
-        
-#          # 对LSTM层输出的所有时间步进行归一化
-#         batch_size_1, seq_len_1, hidden_size_1 = output_1.size()
-#         batch_size_2, seq_len_2, hidden_size_2 = output_2.size()
-#         output_1 = self.layer_norm(output_1.view(batch_size_1 * seq_len_1, hidden_size_1)).view(batch_size_1, seq_len_1, hidden_size_1)
-#         output_2 = self.layer_norm(output_2.view(batch_size_2 * seq_len_2, hidden_size_2)).view(batch_size_2, seq_len_2, hidden_size_2)
 
         context_1 = self.attention(h_n_1[-1].unsqueeze(0), output_2)
         context_2 = self.attention(h_n_2[-1].unsqueeze(0), output_1)
-        
-#         context_1 = self.layer_norm(context_1)
-#         context_2 = self.layer_norm(context_2)
-#         output_1 = torch.sigmoid(self.fc(context_1.squeeze(1)))
-#         output_2 = torch.sigmoid(self.fc(context_2.squeeze(1)))
+    
         output_1 = self.fc1(context_1.squeeze(1))
         output_2 = self.fc2(context_2.squeeze(1))
 
